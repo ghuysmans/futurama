049cadfc80f809a194555d6f8cb6174caf318138
Guillaume Huysmans <ghuysmans99@gmail.com>
2018-12-27 19:32:33 +0100

    Cleanup: Graphviz on stdout, answers on stderr

--- aoc17_day16.ml
+++ aoc17_day16.ml
@@ -59,18 +59,16 @@ let () =
     Fixed.of_list 16 pos, Fixed.of_list 16 chars
   in
   let charset = "abcdefghijklmnop" in
-  Printf.printf "naive: %s\n\n" (List.fold_left step charset script);
-  Printf.printf "fast: %s\n\n" (eval charset ~pos ~chars);
+  Printf.eprintf "naive: %s\n" (List.fold_left step charset script);
+  Printf.eprintf "fast: %s\n" (eval charset ~pos ~chars);
   let pos' = Fixed.to_disjoint_cycles pos in
   let chars' = Fixed.to_disjoint_cycles chars in
   Printf.printf "%a" Dot.header ();
   List.iter (Printf.printf "%a" Dot.(cycle int)) pos';
-  let letter ch i = Printf.fprintf ch "%c" (Char.chr (97 + i)) in
+  let letter ch i = Printf.fprintf ch "%c" charset.[i] in
   List.iter (Printf.printf "%a" Dot.(cycle letter)) chars';
   Printf.printf "%a" Dot.footer ();
-  Printf.printf "pos:\n%a\n" Latex.(list "itemize" (chain int)) pos';
-  Printf.printf "chars:\n%a\n" Latex.(list "itemize" (chain letter)) chars';
   let n = 1_000_000_000 in
   let pos = Disjoint_cycles.pow n pos' |> Fixed.of_disjoint_cycles 16 in
   let chars = Disjoint_cycles.pow n chars' |> Fixed.of_disjoint_cycles 16 in
-  Printf.printf "%d: %s\n\n" n (eval charset ~pos ~chars)
+  Printf.eprintf "%d: %s\n" n (eval charset ~pos ~chars)
