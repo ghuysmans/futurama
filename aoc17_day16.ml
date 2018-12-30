bfe6267210e43836c7185a02fb28648788ac7708
Guillaume Huysmans <ghuysmans99@gmail.com>
2018-12-30 15:55:51 +0100

    Compared different algorithms to find bugs

--- aoc17_day16.ml
+++ aoc17_day16.ml
@@ -50,5 +50,46 @@ let tortoise_and_hare init script n =
 
 let () =
   let script = read_moves () in
+  let n, a =
+    match Sys.argv with
+    | [| _ |] -> 1, `Naive
+    | [| _; n |] -> int_of_string n, `Naive
+    | [| _; "-t"; n |] -> int_of_string n, `Tortoise
+    | [| _; "-c"; n |] -> int_of_string n, `Cycle
+    | [| _; "-p"; n |] | [| _; "-g"; n |] -> int_of_string n, `Pow
+    | _ ->
+      Printf.eprintf "usage: %s [-t|-c|-p] [n]\n" Sys.argv.(0);
+      exit 1
+  in
   let init = "abcdefghijklmnop" in
-  Printf.eprintf "%s\n" (tortoise_and_hare init script 1_000_000_000)
+  let result =
+    match a with
+    | `Naive -> Util.pow (fun x -> List.fold_left step x script) init n
+    | `Tortoise -> tortoise_and_hare init script n
+    | `Cycle | `Pow as a' ->
+      let pos, chars =
+        let f m (pos, chars) =
+          match m with
+          | Spin n -> Fixed.Rotate_right n :: pos, chars
+          | Exchange (i, j) -> Fixed.Transpose (i, j) :: pos, chars
+          | Partner (x, y) ->
+            pos, Fixed.Transpose (Char.code x - 97, Char.code y - 97) :: chars
+        in
+        List.fold_right f script ([], [])
+      in
+      let pos, chars = Fixed.of_list 16 pos, Fixed.of_list 16 chars in
+      let pos, chars =
+        match a' with
+        | `Cycle ->
+          let fast_forward p n =
+            Fixed.to_disjoint_cycles p |>
+            Disjoint_cycles.pow n |>
+            Fixed.of_disjoint_cycles 16
+          in
+          fast_forward pos n, fast_forward chars n
+        | `Pow ->
+          Fixed.pow pos n, Fixed.pow chars n
+      in
+      Fixed.(explain chars init |> transform pos)
+  in
+  Printf.eprintf "%s\n" result
