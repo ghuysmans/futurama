808d94420a21a0720375d0448f5d6fd4eef1d5c0
Guillaume Huysmans <ghuysmans99@gmail.com>
2019-12-28 02:54:51 +0100

    gcc -O4

---
+++ dance/part1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main() {
+	int tmp, v0=0, v1=1, v2=2, v3=3, v4=4, v5=5, v6=6, v7=7, v8=8, v9=9, v10=10, v11=11, v12=12, v13=13, v14=14, v15=15;
+#include "dance.c"
+	printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15);
+}
